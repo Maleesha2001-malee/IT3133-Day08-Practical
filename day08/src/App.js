edd137900c090ed3ad1d59038192f63044aae3de
Maleesha Rathnayake <139684247+Maleesha2001-malee@users.noreply.github.com>
2024-12-02 14:33:10 +0000

    add routes for navigations

--- day08/src/App.js
+++ day08/src/App.js
@@ -8,14 +8,21 @@ function App() {
   return (
     <div className="App">
     <Router>
-      <nav>
+        <nav>
          <ul>
              <li><Link to='/home'>Home</Link></li>
              <li><Link to='/about'>About</Link></li>
              <li><Link to='/contact'>Contact</Link></li>
           </ul>
-      </nav>
+        </nav>
+        <Routes>
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+
+        </Routes>
       </Router>
+
     </div>
 
   );
